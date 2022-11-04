96cac1a0a0d1ed3786906a757ff2eddfa95c85e2
gebib <gebi9@hotmail.com>
2022-11-05 00:14:28 +0100

    BoilerPlate + added all deps / the plugin in pom.xml

---
+++ src/main/java/com/example/a4_spring_boot_soap/A4SpringBootSoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.a4_spring_boot_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A4SpringBootSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A4SpringBootSoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/a4_spring_boot_soap/A4SpringBootSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a4_spring_boot_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A4SpringBootSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
